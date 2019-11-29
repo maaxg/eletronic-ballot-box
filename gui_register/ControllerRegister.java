89cf4aa80708f4fca7812d4493c3024f30181225
maaxg <49838612+maaxg@users.noreply.github.com>
2019-11-29 16:11:19 -0300

    Add files via upload

---
+++ gui_register/ControllerRegister.java
@@ -0,0 +1,63 @@
+package gui_register;
+
+import class_methods.PersonMethods;
+import gui_login.Controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model_person.Person;
+import model_person.dao.PersonDAO;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ControllerRegister implements Initializable {
+    Person pe = new Person();
+    @FXML
+    public TextField txtNome;
+    @FXML
+    public TextField txtCpf;
+    @FXML
+    public TextField txtData_nasc;
+    @FXML
+    public TextField txtLogin_reg;
+    @FXML
+    public PasswordField txtSenha_reg;
+
+
+
+
+    @FXML
+    private void handleConfirmButton(ActionEvent e){
+        Controller c = new Controller();
+        Person pe = new Person(1,txtNome.getText(), txtData_nasc.getText(), txtCpf.getText(), txtLogin_reg.getText(), txtSenha_reg.getText());
+        PersonDAO dao = new PersonDAO();
+        try {
+            dao.save(pe);
+            JOptionPane.showMessageDialog(null,"Sucessfull registration");
+
+        }catch(Exception exc){
+            exc.printStackTrace();
+        }finally {
+            c.stage_reg.close();
+        }
+       
+        
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
